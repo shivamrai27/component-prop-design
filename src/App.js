7c62f9df3171b7da490f840992218bfd86f03fdd
Shivam Rai <200shivamrai@gmail.com>
2023-09-03 02:31:11 +0530

    react js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+import img from './img.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={img} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You <code>are </code>watching masters of work.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/shivamrai27"
           target="_blank"
           rel="noopener noreferrer"
+
         >
-          Learn React
+          5S
         </a>
       </header>
     </div>
